b197c19d1c27b937e8399029a00f4605bbe18a14
taishi <39787700+taiseaocean@users.noreply.github.com>
2020-11-24 12:21:07 -0800

    Update general_info.R

--- scripts/general_info.R
+++ scripts/general_info.R
@@ -4,14 +4,6 @@ library(skimr)
 # A function that takes in a dataset and returns a list of infor about it:
 
 
-capetown <- read.csv("data/CapeTown_2020_October/listings.csv", stringsAsFactors = FALSE)
-london <- read.csv("data/London_2020_October/listings.csv", stringsAsFactors = FALSE)
-melbourne <- read.csv("data/Melbourne_2020_October/listings.csv", stringsAsFactors = FALSE)
-mexico <- read.csv("data/MexicoCity_2020_October/listings.csv", stringsAsFactors = FALSE)
-seattle <- read.csv("data/Seattle_2020_October/listings.csv", stringsAsFactors = FALSE)
-tokyo <- read.csv("data/Tokyo_2020_October/listings.csv", stringsAsFactors = FALSE)
-
-
 get_summary_info <- function(dataset) {
   ret <- list()
   ret$length <- length(dataset)
@@ -25,6 +17,8 @@ get_summary_info <- function(dataset) {
   return(ret)
 }
 
+
+# use View() to check description
 tokyo_listing_overview <- get_summary_info(tokyo)
 seattle_listing_overview <- get_summary_info(seattle)
 mexico_listing_overview <- get_summary_info(mexico)
