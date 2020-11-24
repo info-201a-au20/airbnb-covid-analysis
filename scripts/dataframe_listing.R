5f7d276ce04ef6d7b7a5950553d7bcb475d9ab47
taishi <39787700+taiseaocean@users.noreply.github.com>
2020-11-24 12:21:10 -0800

    Create dataframe_listing.R

---
+++ scripts/dataframe_listing.R
@@ -0,0 +1,6 @@
+capetown <- read.csv("data/CapeTown_2020_October/listings.csv", stringsAsFactors = FALSE)
+london <- read.csv("data/London_2020_October/listings.csv", stringsAsFactors = FALSE)
+melbourne <- read.csv("data/Melbourne_2020_October/listings.csv", stringsAsFactors = FALSE)
+mexico <- read.csv("data/MexicoCity_2020_October/listings.csv", stringsAsFactors = FALSE)
+seattle <- read.csv("data/Seattle_2020_October/listings.csv", stringsAsFactors = FALSE)
+tokyo <- read.csv("data/Tokyo_2020_October/listings.csv", stringsAsFactors = FALSE)
