38364fac4684b6207df4ef2f23ce3626ad1d2950
taishi <39787700+taiseaocean@users.noreply.github.com>
2020-11-24 12:32:27 -0800

    Create dataframe_review.R

---
+++ scripts/dataframe_review.R
@@ -0,0 +1,13 @@
+
+
+capetown_reviews <- read.csv("data/CapeTown_2020_October/reviews.csv", stringsAsFactors = FALSE)
+
+london_reviews <- read.csv("data/London_2020_October/reviews.csv", stringsAsFactors = FALSE)
+
+melbourne_reviews <- read.csv("data/Melbourne_2020_October/reviews.csv", stringsAsFactors = FALSE)
+
+mexico_reviews <- read.csv("data/MexicoCity_2020_October/reviews.csv", stringsAsFactors = FALSE)
+
+seattle_reviews <- read.csv("data/Seattle_2020_October/reviews.csv", stringsAsFactors = FALSE)
+
+tokyo_reviews<- read.csv("data/Tokyo_2020_October/reviews.csv", stringsAsFactors = FALSE)
