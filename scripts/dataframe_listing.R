807d2ce0791f83b70a5b5c847b73e813aec44108
taishi <39787700+taiseaocean@users.noreply.github.com>
2020-11-24 12:32:34 -0800

    Update dataframe_listing.R

--- scripts/dataframe_listing.R
+++ scripts/dataframe_listing.R
@@ -1,6 +1,14 @@
-capetown <- read.csv("data/CapeTown_2020_October/listings.csv", stringsAsFactors = FALSE)
-london <- read.csv("data/London_2020_October/listings.csv", stringsAsFactors = FALSE)
-melbourne <- read.csv("data/Melbourne_2020_October/listings.csv", stringsAsFactors = FALSE)
-mexico <- read.csv("data/MexicoCity_2020_October/listings.csv", stringsAsFactors = FALSE)
-seattle <- read.csv("data/Seattle_2020_October/listings.csv", stringsAsFactors = FALSE)
-tokyo <- read.csv("data/Tokyo_2020_October/listings.csv", stringsAsFactors = FALSE)
+
+
+capetown_listing <- read.csv("data/CapeTown_2020_October/listings.csv", stringsAsFactors = FALSE)
+
+london_listing <- read.csv("data/London_2020_October/listings.csv", stringsAsFactors = FALSE)
+
+melbourne_listing <- read.csv("data/Melbourne_2020_October/listings.csv", stringsAsFactors = FALSE)
+
+mexico_listing <- read.csv("data/MexicoCity_2020_October/listings.csv", stringsAsFactors = FALSE)
+
+seattle_listing <- read.csv("data/Seattle_2020_October/listings.csv", stringsAsFactors = FALSE)
+
+tokyo_listing <- read.csv("data/Tokyo_2020_October/listings.csv", stringsAsFactors = FALSE)
+
