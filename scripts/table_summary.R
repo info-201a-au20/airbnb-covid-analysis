6889ba48ea35e132cf8eccbf650176c87cf6a3f2
taishi <39787700+taiseaocean@users.noreply.github.com>
2020-11-24 18:50:03 -0800

    Create table_summary.R

---
+++ scripts/table_summary.R
@@ -0,0 +1,16 @@
+library("dplyr")
+
+
+### Summary Table
+
+# get dataset
+seattle_listing <- read.csv("data/Seattle_2020_October/listings.csv", stringsAsFactors = FALSE)
+
+
+# function to get summary table for seattle listing dataset since it has overview info
+# - Well formated column name
+# - only conains relevant info
+# - Be interntionally sorted in a meaningful way
+
+
+summary_table <- get_summary_table
