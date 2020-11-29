2ea1bc359872dfec6312677c7fc140ff408adc54
taishi <39787700+taiseaocean@users.noreply.github.com>
2020-11-28 21:17:18 -0800

    Update dataframe_neighbor.R

--- scripts/dataframe_neighbor.R
+++ scripts/dataframe_neighbor.R
@@ -1,13 +1,5 @@
+## Will use for the next analysis
 
+# seattle_neighbor <- read.csv("../data/Seattle_2020_October/neighbourhoods.csv", stringsAsFactors = FALSE)
 
-capetown_neighbor <- read.csv("data/CapeTown_2020_October/neighbourhoods.csv", stringsAsFactors = FALSE)
-
-london_neighbor <- read.csv("data/London_2020_October/neighbourhoods.csv", stringsAsFactors = FALSE)
-
-melbourne_neighbor <- read.csv("data/Melbourne_2020_October/neighbourhoods.csv", stringsAsFactors = FALSE)
-
-mexico_neighbor <- read.csv("data/MexicoCity_2020_October/neighbourhoods.csv", stringsAsFactors = FALSE)
-
-seattle_neighbor <- read.csv("../data/Seattle_2020_October/neighbourhoods.csv", stringsAsFactors = FALSE)
-
-tokyo_neighbor <- read.csv("data/Tokyo_2020_October/neighbourhoods.csv", stringsAsFactors = FALSE)
+# tokyo_neighbor <- read.csv("data/Tokyo_2020_October/neighbourhoods.csv", stringsAsFactors = FALSE)
