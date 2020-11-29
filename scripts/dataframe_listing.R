57e537e97c779dcb364063440451765cdb669680
taishi <39787700+taiseaocean@users.noreply.github.com>
2020-11-28 21:16:46 -0800

    Update dataframe_listing.R

--- scripts/dataframe_listing.R
+++ scripts/dataframe_listing.R
@@ -1,14 +1,8 @@
 
+seattle_listings <- read.csv("data/Seattle_2020_October/listings.csv", stringsAsFactors = FALSE)
+# data cleaning
+seattle_listings$price <- as.numeric(sub("\\$","", seattle_listings$price))
 
-capetown_listing <- read.csv("../data/CapeTown_2020_October/listings.csv", stringsAsFactors = FALSE)
-
-london_listing <- read.csv("../data/London_2020_October/listings.csv", stringsAsFactors = FALSE)
-
-melbourne_listing <- read.csv("../data/Melbourne_2020_October/listings.csv", stringsAsFactors = FALSE)
-
-mexico_listing <- read.csv("../data/MexicoCity_2020_October/listings.csv", stringsAsFactors = FALSE)
-
-seattle_listing <- read.csv("../data/Seattle_2020_October/listings.csv", stringsAsFactors = FALSE)
-
-tokyo_listing <- read.csv("../data/Tokyo_2020_October/listings.csv", stringsAsFactors = FALSE)
+## Will use for the next analysis
+# tokyo_listings <- read.csv("../data/Tokyo_2020_October/listings.csv", stringsAsFactors = FALSE)
 
