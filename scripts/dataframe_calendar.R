dcfe270b20892457ce5a9bd9ab9d195e768b1aad
taishi <39787700+taiseaocean@users.noreply.github.com>
2020-11-28 21:16:17 -0800

    Update dataframe_calendar.R

--- scripts/dataframe_calendar.R
+++ scripts/dataframe_calendar.R
@@ -1,16 +1,5 @@
+# will use next analysis
 
+# seattle_calendar <- read.csv("../data/Seattle_2020_October/calendar.csv.gz", stringsAsFactors = FALSE)
 
-capetown_calendar <- read.csv("data/CapeTown_2020_October/calendar.csv.gz", stringsAsFactors = FALSE)
-
-london_calendar <- read.csv("data/London_2020_October/calendar.csv.gz", stringsAsFactors = FALSE)
-
-melbourne_calendar <- read.csv("data/Melbourne_2020_October/calendar.csv.gz", stringsAsFactors = FALSE)
-
-mexico_calendar <- read.csv("data/MexicoCity_2020_October/calendar.csv.gz", stringsAsFactors = FALSE)
-
-seattle_calendar <- read.csv("../data/Seattle_2020_October/calendar.csv.gz", stringsAsFactors = FALSE)
-
-tokyo_calendar <- read.csv("data/Tokyo_2020_October/calendar.csv.gz", stringsAsFactors = FALSE)
-
-
-View(capetown_calendar)
+# tokyo_calendar <- read.csv("data/Tokyo_2020_October/calendar.csv.gz", stringsAsFactors = FALSE)
