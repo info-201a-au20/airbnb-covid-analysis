0a468371ea5392b3aa6582ae66bd1151b0cc6641
taishi <39787700+taiseaocean@users.noreply.github.com>
2020-12-08 00:42:28 -0800

    Update dataframe_listing.R

--- scripts/dataframe_listing.R
+++ scripts/dataframe_listing.R
@@ -1,8 +1,17 @@
 
-seattle_listings <- read.csv("data/Seattle_2020_October/listings.csv", stringsAsFactors = FALSE)
-# data cleaning
-seattle_listings$price <- as.numeric(sub("\\$","", seattle_listings$price))
+get_listings <- function(dataframe) {
+  current_dataframe <- read.csv(dataframe, stringsAsFactors = FALSE) 
+  current_dataframe$price <- as.numeric(sub("\\$","", current_dataframe$price))
+  
+  return(current_dataframe)
+  
+}
 
-## Will use for the next analysis
-# tokyo_listings <- read.csv("../data/Tokyo_2020_October/listings.csv", stringsAsFactors = FALSE)
-
+seattle_listings <- suppressWarnings(get_listings("data/Seattle_2020_October/listings.csv.gz"))
+tokyo_listings <- suppressWarnings(get_listings("data/Tokyo_2020_October/listings.csv.gz"))
+mexico_listings <- suppressWarnings(get_listings("data/MexicoCity_2020_October/listings.csv.gz"))
+melbourne_listings <- suppressWarnings(get_listings("data/Melbourne_2020_October/listings.csv.gz"))
+london_listings <- suppressWarnings(get_listings("data/London_2020_October/listings.csv.gz"))
+capetown_listings <- suppressWarnings(get_listings("data/CapeTown_2020_October/listings.csv.gz"))
+sanfrancisco_listings <- suppressWarnings(get_listings("data/SanFrancisco_2020_October/listings.csv.gz"))
+newyork_listings <- suppressWarnings(get_listings("data/NewYork_2020_October/listings.csv.gz"))
