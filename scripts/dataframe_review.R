30828cb255633c172ae17cc35e460f079d9be0a2
taishi <39787700+taiseaocean@users.noreply.github.com>
2020-12-08 00:42:37 -0800

    Update dataframe_review.R

--- scripts/dataframe_review.R
+++ scripts/dataframe_review.R
@@ -1,6 +1,16 @@
 
 
-seattle_reviews <- read.csv("data/Seattle_2020_October/reviews.csv", stringsAsFactors = FALSE)
+seattle_reviews <- read.csv("data/Seattle_2020_October/reviews.csv.gz", stringsAsFactors = FALSE)
 
-## Will use for the next analysis
-# tokyo_reviews<- read.csv("../data/Tokyo_2020_October/reviews.csv", stringsAsFactors = FALSE)
+tokyo_reviews<- read.csv("data/Tokyo_2020_October/reviews.csv.gz", stringsAsFactors = FALSE)
+sanfrancisco_reviews <- read.csv("data/SanFrancisco_2020_October/reviews.csv.gz", stringsAsFactors = FALSE)
+newyork_reviews <-read.csv("data/NewYork_2020_October/reviews.csv.gz", stringsAsFactors = FALSE)
+mexicocity_reviews <- read.csv("data/MexicoCity_2020_October/reviews.csv.gz", stringsAsFactors = FALSE)
+melbourne_reviews <- read.csv("data/Melbourne_2020_October/reviews.csv.gz", stringsAsFactors = FALSE)
+london_reviews <- read.csv("data/London_2020_October/reviews.csv.gz", stringsAsFactors = FALSE)
+capetown_reviews <- read.csv("data/CapeTown_2020_October/reviews.csv.gz", stringsAsFactors = FALSE)
+
+
+
+
+
