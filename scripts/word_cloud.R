5d91107f7e1866e4b4299d4276cb47706459e15d
taishi <39787700+taiseaocean@users.noreply.github.com>
2020-12-09 20:49:40 -0800

    Update word_cloud.R

--- scripts/word_cloud.R
+++ scripts/word_cloud.R
@@ -1,46 +1,56 @@
 
+
 library(wordcloud2)
 
 get_cloud_visual_listings <- function(dataset) {
-  
   finalWords <- dataset %>%
     select(id, description, price, name) %>%
     unnest_tokens(word, description) %>%
     filter(!word %in% stop_words$word,
            str_detect(word, "^[a-z]+$"))
   
-  finalCloud <- as.data.frame(finalWords %>%
-                                group_by(word) %>%
-                                filter(!word == "br") %>%
-                                summarise(word_counter = length(word)))
-  finalCloud2 <- finalCloud[1: 200, ]
+  finalCloud <- as.data.frame(
+    finalWords %>%
+      group_by(word) %>%
+      filter(!word == "br") %>%
+      summarise(word_counter = length(word))
+  )
+  finalCloud2 <- finalCloud[1:200,]
   
-  wordcloud2(data = finalCloud,
-              color = 'random-dark', backgroundColor = "black", widgetsize = 1.5)
+  wordcloud2(
+    data = finalCloud,
+    color = 'random-dark',
+    backgroundColor = "black",
+    widgetsize = 1.5
+  )
   
-
+  
 }
 
 
 
 
 get_cloud_visual_reviews <- function(dataset) {
-  
   finalWords <- dataset %>%
     select(id, comments) %>%
     unnest_tokens(word, comments) %>%
     filter(!word %in% stop_words$word,
            str_detect(word, "^[a-z]+$"))
   
-  finalCloud <- as.data.frame(finalWords %>%
-                                group_by(word) %>%
-                                filter(!word == "br") %>%
-                                summarise(word_counter = length(word)))
-  finalCloud2 <- finalCloud[1: 200, ]
+  finalCloud <- as.data.frame(
+    finalWords %>%
+      group_by(word) %>%
+      filter(!word == "br") %>%
+      summarise(word_counter = length(word))
+  )
+  finalCloud2 <- finalCloud[1:200,]
   
-  wordcloud2(data = finalCloud,
-             color = 'random-dark', backgroundColor = "black", widgetsize = 1.5)
+  wordcloud2(
+    data = finalCloud,
+    color = 'random-dark',
+    backgroundColor = "black",
+    widgetsize = 1.5
+  )
   
   
 }
-
