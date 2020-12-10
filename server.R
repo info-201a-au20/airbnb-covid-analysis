a33a60fa9e983f5539c16662e8c91d9946dce273
taishi <39787700+taiseaocean@users.noreply.github.com>
2020-12-09 20:43:33 -0800

    Update server.R

--- server.R
+++ server.R
@@ -10,12 +10,9 @@ library(dplyr)
 library(plotly)
 library(ggplot2)
 
-
 source("scripts/dataframe_review.R")
 source("scripts/dataframe_listing.R")
 source("scripts/table_summary.R")
-#source("scripts/general_info.R")
-#source("scripts/table_summary.R")
 source("visualizer/chart1.R")
 source("visualizer/chart2.R")
 source("visualizer/chart3.R")
@@ -112,49 +109,9 @@ server <- shinyServer(function(input, output) {
       }
     }
     
-    # color setting for the circle maker, color changes depends on minimum night stay.
-    color_setting <-
-      colorNumeric("YlGnBu", domain = matched_dataframe$minimum_nights)
-    # this function will create leaflet that shows geographic representation of Airbnb's location in Seattle.
-    # the matched_dataframe is used as dataset and popup includes short description and link to the airbnb site.
-    chart3 <- leaflet(data = matched_dataframe) %>%
-      addProviderTiles(providers$CartoDB.DarkMatter, group = "Dark") %>%
-      addProviderTiles(providers$CartoDB.Positron, group = "Light") %>%
-      addCircleMarkers(
-        ~ longitude,
-        ~ latitude,
-        color = ~ color_setting(minimum_nights),
-        stroke = FALSE,
-        # SF has too huge minimum stay integer so adjust it
-        radius = if (input$current_country == "San Francisco" &&
-                     input$filter_yes_or_no == "No") {
-          ~ minimum_nights / 1000000
-        } else {
-          ~ minimum_nights / 5
-        },
-        fillOpacity = ~ minimum_nights / 10,
-        popup = paste0(
-          "<b> You have to stay here at least: </b>",
-          matched_dataframe$minimum_nights,
-          "days!",
-          "<br><b> Overview: </b></br>",
-          matched_dataframe$description,
-          "<a href='",
-          matched_dataframe$listing_url,
-          "' target =' _blank'>",
-          "<br>Click Here for more details</a></br>",
-          sep = "<br/>"
-        )
-      ) %>%
-      
-      addLegend(
-        "bottomright",
-        pal = color_setting,
-        values = ~ minimum_nights,
-        title = "Minimum Nights",
-        opacity = 1
-      )
-    return(chart3)
+ 
+    airbnbMap <- getMap(matched_dataframe)
+    return(airbnbMap)
     
   })
   
