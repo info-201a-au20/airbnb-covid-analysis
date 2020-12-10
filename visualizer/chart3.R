1af9cbd0c1ff4fa6aa8a77631e4a8c4ca19d3ff0
taishi <39787700+taiseaocean@users.noreply.github.com>
2020-12-09 20:43:20 -0800

    Update chart3.R

--- visualizer/chart3.R
+++ visualizer/chart3.R
@@ -24,23 +24,31 @@ matched_dataframe <- get_filtered_reviews(seattle_reviews, seattle_listings)
 
 
 # color setting for the circle maker, color changes depends on minimum night stay.
-color_setting <- colorNumeric("YlGnBu", domain = matched_dataframe$minimum_nights)
+# color_setting <- colorNumeric("YlGnBu", domain = matched_dataframe$minimum_nights)
 # this function will create leaflet that shows geographic representation of Airbnb's location in Seattle.
 # the matched_dataframe is used as dataset and popup includes short description and link to the airbnb site.
-chart3 <- leaflet(data = matched_dataframe) %>%
-  addProviderTiles(providers$CartoDB.DarkMatter, group="Dark") %>%
-  addProviderTiles(providers$CartoDB.Positron, group="Light") %>%
-  addCircleMarkers(~longitude,
-             ~latitude,
-             color = ~color_setting(minimum_nights),
-             stroke = FALSE,
-             radius = ~minimum_nights / 5, 
-             fillOpacity = ~minimum_nights/10,
-             popup = paste0("<b> You have to stay here at least: </b>", matched_dataframe$minimum_nights, "days!",
-                            "<br><b> Overview: </b></br>", matched_dataframe$description,
-                            "<a href='", matched_dataframe$listing_url, "' target =' _blank'>", 
-                            "<br>Click Here for more details</a></br>", sep = "<br/>")) %>% 
+
+getMap <- function(matched_dataframe) {
   
-  addLegend("bottomright", pal = color_setting,
-            values = ~minimum_nights, title = "Minimum Nights", opacity = 1)
+  color_setting <- colorNumeric("YlGnBu", domain = matched_dataframe$minimum_nights)
+  
+  leaflet(data = matched_dataframe) %>%
+    addProviderTiles(providers$CartoDB.DarkMatter, group = "Dark") %>%
+    addProviderTiles(providers$CartoDB.Positron, group = "Light") %>%
+    addCircleMarkers(~longitude,
+                     ~latitude,
+                     color = ~color_setting(minimum_nights),
+                     stroke = FALSE,
+                     radius = ~minimum_nights / 5, 
+                     fillOpacity = ~minimum_nights/10,
+                     popup = paste0("<b> You have to stay here at least: </b>", matched_dataframe$minimum_nights, "days!",
+                                    "<br><b> Overview: </b></br>", matched_dataframe$description,
+                                    "<a href='", matched_dataframe$listing_url, "' target =' _blank'>", 
+                                    "<br>Click Here for more details</a></br>", sep = "<br/>")) %>% 
+    
+    addLegend("bottomright", pal = color_setting,
+              values = ~minimum_nights, title = "Minimum Nights", opacity = 1)
+  
+
+}
 
